2ec65027e3544391a442a9b91044ba99a7c831ec
Lanre-Awe <lanreawe7@gmail.com>
2023-05-13 20:02:14 +0200

    steve

---
+++ src/database/storeFiles.js
@@ -0,0 +1,311 @@
+export const storeData = [
+  {
+    category: "console",
+    games: [
+      {
+        title: "Call Of Duty Warzone",
+        banner: "",
+        gameStore: [
+          {
+            price: `${100} CP`,
+            image:
+              "https://image.api.playstation.com/vulcan/ap/rnd/202211/0300/kiED3wRWEveOyGRuZiGJgW9B.png",
+          },
+          {
+            price: `${500} CP`,
+            image:
+              "https://imgs.callofduty.com/content/dam/atvi/callofduty/cod-touchui/mw2/seasons/common/mw2-cp-back-bg-desktop.jpg",
+          },
+          {
+            price: `${1000} CP`,
+            image:
+              "https://store-images.s-microsoft.com/image/apps.6458.13631926718666261.0880c043-8de6-49b4-bb63-bdee9e896563.045065a4-7477-465a-bf33-ed9b66fd99d3?q=90&w=480&h=270",
+          },
+          {
+            price: `${1250} CP`,
+            image:
+              "https://blz-contentstack-images.akamaized.net/v3/assets/bltf408a0557f4e4998/bltdf316251c5335a01/6362d4c4f17951383bcfbbe4/CODHQ_COD_Points_13000-Bnet_Game-Product_Browsing_Card-960x540.jpg?width=&format=webply&dpr=2&disable=upscale&quality=80",
+          },
+        ],
+        quickSale: [
+          {
+            price: `${100} CP`,
+            image:
+              "https://image.api.playstation.com/vulcan/ap/rnd/202211/0300/kiED3wRWEveOyGRuZiGJgW9B.png",
+          },
+          {
+            price: `${1000} CP`,
+            image:
+              "https://store-images.s-microsoft.com/image/apps.6458.13631926718666261.0880c043-8de6-49b4-bb63-bdee9e896563.045065a4-7477-465a-bf33-ed9b66fd99d3?q=90&w=480&h=270",
+          },
+        ],
+      },
+      {
+        title: "Fifa",
+        banner: "",
+        gameStore: [
+          {
+            price: `${2500} COIN`,
+            image:
+              "https://i0.wp.com/futballnews.com/wp-content/uploads/2020/08/FB33294E-3E17-4F89-B3E1-F2DDD90ED735.jpeg?resize=490%2C351&ssl=1",
+          },
+          {
+            price: `${5000} COIN`,
+            image:
+              "https://i0.wp.com/futballnews.com/wp-content/uploads/2020/08/FB33294E-3E17-4F89-B3E1-F2DDD90ED735.jpeg?resize=490%2C351&ssl=1",
+          },
+          {
+            price: `${10000} COIN`,
+            image:
+              "https://prod.assets.earlygamecdn.com/images/FIFA-Coin-Mechanics-Explained.png.jpg?x=0.5&y=0.5",
+          },
+          {
+            price: `${12500} COIN`,
+            image:
+              "https://prod.assets.earlygamecdn.com/images/FIFA-Coin-Mechanics-Explained.png.jpg?x=0.5&y=0.5",
+          },
+        ],
+        quickSale: [
+          {
+            price: `${5000} COIN`,
+            image:
+              "https://i0.wp.com/futballnews.com/wp-content/uploads/2020/08/FB33294E-3E17-4F89-B3E1-F2DDD90ED735.jpeg?resize=490%2C351&ssl=1",
+          },
+          {
+            price: `${12500} COIN`,
+            image:
+              "https://prod.assets.earlygamecdn.com/images/FIFA-Coin-Mechanics-Explained.png.jpg?x=0.5&y=0.5",
+          },
+        ],
+      },
+      {
+        title: "Fortnite",
+        banner: "",
+        gameStore: [
+          {
+            price: `${250} VBUCKS`,
+            image:
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5vwTLX_S-bNX5pr20ccOc9VTpT0E3_V_ZGiPCugAhiD2t38yoO5R3e9nyK-PhawVrhUw&usqp=CAU",
+          },
+          {
+            price: `${500} VBUCKS`,
+            image:
+              "https://www.cnet.com/a/img/resize/1de81ea03d63e7674af735c930b1e62e59a7e09b/hub/2020/02/14/676146ec-f899-4c73-a132-99f7bff87827/vbucks.png?auto=webp&fit=crop&height=675&width=1200",
+          },
+          {
+            price: `${1000} VBUCKS`,
+            image: "https://i.insider.com/5bd9f4860591f23a4b521857?width=700",
+          },
+          {
+            price: `${1250} VBUCKS`,
+            image:
+              "https://static.wikia.nocookie.net/fortnite/images/4/44/5%2C000_V-Bucks_-_Shop_Tile_-_Fortnite.png/revision/latest/scale-to-width-down/250?cb=20210222174354",
+          },
+        ],
+        quickSale: [
+          {
+            price: `${500} VBUCKS`,
+            image:
+              "https://www.cnet.com/a/img/resize/1de81ea03d63e7674af735c930b1e62e59a7e09b/hub/2020/02/14/676146ec-f899-4c73-a132-99f7bff87827/vbucks.png?auto=webp&fit=crop&height=675&width=1200",
+          },
+          {
+            price: `${1250} VBUCKS`,
+            image:
+              "https://gamecardsdirect.com/content/image/Fortnite/fortnite-v-bucks.png",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    category: "Giftcard",
+    games: [
+      {
+        title: "Apple Giftcard",
+        banner: "",
+        gameStore: [
+          {
+            price: `${25} USD`,
+          },
+          {
+            price: `${50} USD`,
+          },
+          {
+            price: `${75} USD`,
+          },
+          {
+            price: `${100} USD`,
+          },
+        ],
+        quickSale: [
+          {
+            price: `${25} USD`,
+          },
+          {
+            price: `${100} USD`,
+          },
+        ],
+      },
+      {
+        title: "Playstation Giftcard",
+        banner: "",
+        gameStore: [
+          {
+            price: `${15} USD`,
+          },
+          {
+            price: `${25} USD`,
+          },
+          {
+            price: `${50} USD`,
+          },
+          {
+            price: `${75} USD`,
+          },
+        ],
+        quickSale: [
+          {
+            price: `${15} USD`,
+          },
+          {
+            price: `${25} USD`,
+          },
+        ],
+      },
+      {
+        title: "Xbox Giftcard",
+        banner: "",
+        gameStore: [
+          {
+            price: `${10} USD`,
+          },
+          {
+            price: `${15} USD`,
+          },
+          {
+            price: `${25} USD`,
+          },
+          {
+            price: `${50} USD`,
+          },
+        ],
+        quickSale: [
+          {
+            price: `${25} USD`,
+          },
+          {
+            price: `${50} USD`,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    category: "mobile",
+    games: [
+      {
+        title: "Call Of Duty Mobile",
+        banner: "",
+        gameStore: [
+          {
+            price: `${250} CP`,
+          },
+          {
+            price: `${100} CP`,
+          },
+          {
+            price: `${1000} CP`,
+          },
+          {
+            price: `${2500} CP`,
+          },
+        ],
+        quickSale: [
+          {
+            price: `${25} CP`,
+          },
+          {
+            price: `${1000} CP`,
+          },
+        ],
+      },
+      {
+        title: "Fifa Mobile",
+        banner: "",
+        gameStore: [
+          {
+            price: `${1000} COIN`,
+          },
+          {
+            price: `${2500} COIN`,
+          },
+          {
+            price: `${10000} COIN`,
+          },
+          {
+            price: `${25000} COIN`,
+          },
+        ],
+        quickSale: [
+          {
+            price: `${2500} CP`,
+          },
+          {
+            price: `${25000} CP`,
+          },
+        ],
+      },
+      {
+        title: "Fortnite Mobile",
+        banner: "",
+        gameStore: [
+          {
+            price: `${100} VBUCKS`,
+          },
+          {
+            price: `${500} VBUCKS`,
+          },
+          {
+            price: `${1000} VBUCKS`,
+          },
+          {
+            price: `${1250} VBUCKS`,
+          },
+        ],
+        quickSale: [
+          {
+            price: `${100} VBUCKS`,
+          },
+          {
+            price: `${500} VBUCKS`,
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export const featured = [
+  {
+    title: "Apple Giftcards",
+    category: "Giftcard",
+    image:
+      "https://dundle.com/resources/images/products/product-google-search/itunes-4x3.png",
+    description: "you can purchase your apple giftcards here",
+  },
+  {
+    title: "Fortnite V-Bucks",
+    category: "Console",
+    image:
+      "https://cdn1.epicgames.com/fn/offer/EGS_2800VBucks_1920x1080-1920x1080-eb673b1a86e31de680720a1a35446f04adf987ea.png",
+    description: "you can purchase your Playstation Games Bucks here",
+  },
+  {
+    title: "Call of Duty CP",
+    category: "Mobile",
+    image:
+      "https://www.u-buy.com.ng/productimg/?image=aHR0cHM6Ly9pLmViYXlpbWcuY29tL2ltYWdlcy9nL1NXd0FBT1N3RTFkZ2hZb1kvcy1sNTAwLmpwZw.jpg",
+    description:
+      "you can purchase your call of duty mobile cp on this website ",
+  },
+];
